8f9223be5c61e5240848de76867d0e5f3afe1b58
Tinotenda Kurimwi <tinokurimwi@motech.dev>
2024-07-12 21:14:51 +0000

    solution to the first task

--- customer_layby/public/js/custom_customer_script.js
+++ customer_layby/public/js/custom_customer_script.js
@@ -19,12 +19,13 @@ frappe.ui.form.on('Customer', {
 
 function check_lay_by_eligibility(frm) {
     frm.set_value('custom_layby_eligible', false);
+    // 9 indicates the smallest the phone number can be, as it can be greater with the use of the country code (27)
     if (frm.doc.custom_verification_type === 'ID') {
-        if (frm.doc.custom_id_number && frm.doc.custom_id_number.length === 13 && frm.doc.mobile_no && frm.doc.mobile_no.length > 8) {
+        if (frm.doc.custom_id_number && frm.doc.custom_id_number.length === 13 && frm.doc.mobile_no && frm.doc.mobile_no.length > 9) {
             frm.set_value('custom_layby_eligible', true);
         }
     } else if (frm.doc.custom_verification_type === 'Passport') {
-        if (frm.doc.custom_passport_number && frm.doc.custom_passport_country && frm.doc.mobile_no && frm.doc.mobile_no.length > 8) {
+        if (frm.doc.custom_passport_number && frm.doc.custom_passport_country && frm.doc.mobile_no && frm.doc.mobile_no.length > 9) {
             frm.set_value('custom_layby_eligible', true);
         }
     }
